506160af9985a5a616d67b5e8c1dcb9a5d23ad63
MiyanoX <MiyanoX@users.noreply.github.com>
2022-10-12 18:29:51 +0900

    19

--- Two Pointer/19.py
+++ Two Pointer/19.py
@@ -5,17 +5,17 @@
 #         self.next = next
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        start = ListNode(next=head)
-        head_prev = start
-        end = head
-        
-        while n > 1:
+        start, end = head, head
+
+        for _ in range(n):
             end = end.next
-            n -= 1
             
-        while end.next != None:
+        if not end:
+            return head.next
+
+        while end.next:
             start, end = start.next, end.next
         
         start.next = start.next.next
         
-        return head_prev.next
+        return head
