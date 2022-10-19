80f04d35a915d53e942d70cfdd095c729402d30e
MiyanoX <MiyanoX@users.noreply.github.com>
2022-10-19 21:10:17 +0900

    136

--- Bit Manipulation/136 - Single Number.py
+++ Bit Manipulation/136 - Single Number.py
@@ -2,5 +2,5 @@ class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         res = 0
         for num in nums:
-            res ^= num
+            res ^= num # num ^ num = 0, 0 ^ num = num
         return res
